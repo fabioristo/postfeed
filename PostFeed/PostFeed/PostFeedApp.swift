01029b44f7c94fb2a5b68a4fb19649c6d5422fba
Fabio Ristoratore <fabio.ristoratore@noovle.com>
2023-02-14 10:29:52 +0100

    First commit

---
+++ PostFeed/PostFeed/PostFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostFeedApp.swift
+//  PostFeed
+//
+//  Created by Fabio Ristoratore on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct PostFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
